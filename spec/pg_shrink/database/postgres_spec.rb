fe09ab3425a891eabf6a0ea4390ec350a835a68e
Kevin Ball <kmball11@gmail.com>
2014-05-22 14:58:53 -0700

    update style of postgres spec

--- spec/pg_shrink/database/postgres_spec.rb
+++ spec/pg_shrink/database/postgres_spec.rb
@@ -11,64 +11,52 @@ describe PgShrink::Database::Postgres do
     PgSpecHelper.clear_table(:test_table)
   end
 
-  it "should be creatable and set up a Sequel connection" do
-    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink')
-    db.connection.is_a?(Sequel::Postgres::Database).should == true
-  end
-
-  it "should be able to fetch records in batches" do
-    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :batch_size => 5, :user => 'postgres')
-    (1..20).each do |i|
-      db.connection.run("insert into test_table (name, test) values ('test', #{i})")
+  context "A simple postgres database" do
+    let(:db) do
+      PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :batch_size => 5, :user => 'postgres')
     end
-    batches = []
-    batch_sizes = []
-    db.records_in_batches(:test_table) do |batch|
-      batch_sizes << batch.size
+    it "sets up a Sequel connection" do
+      expect(db.connection.class).to eq(Sequel::Postgres::Database)
     end
-    batch_sizes.should == [5, 5, 5, 5]
-  end
+    context "with 20 simple records" do
+      before(:each) do
+        (1..20).each do |i|
+          db.connection.run("insert into test_table (name, test) values ('test', #{i})")
+        end
+      end
+      it "can fetch records in batches" do
+        batches = []
+        batch_sizes = []
+        db.records_in_batches(:test_table) do |batch|
+          batch_sizes << batch.size
+        end
+        expect(batch_sizes).to eq([5, 5, 5, 5])
+      end
+      it "can delete records based on a condition" do
+        db.delete_records(:test_table, {:test => 1..5})
 
-  it "should be able to delete records based on a condition" do
-    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :user => 'postgres')
-    (1..20).each do |i|
-      db.connection.run("insert into test_table (name, test) values ('test', #{i})")
+        results = db.connection["select * from test_table"].all
+        expect(results.size).to eq(15)
+        expect(results.map {|r| r[:test]}.sort).to eq((6..20).to_a.sort)
+      end
+      it "can able to update records" do
+        old_records = db.connection["select * from test_table where test <= 5"].all
+        new_records = old_records.map {|r| r.merge(:test => r[:test] * 10)}
+        db.update_records('test_table', old_records, new_records)
+        expect(db.connection["select * from test_table where test <= 5"].all.size).to eq(0)
+        updated_records = db.connection[:test_table].where(:id => old_records.map {|r| r[:id]}).all
+        expect(updated_records.size).to eq(old_records.size)
+        expect(updated_records).to eq(new_records)
+      end
+      it "can handle deletions in update records" do
+        old_records = db.connection["select * from test_table where test <= 5"].all
+        new_records = old_records[0..1]
+        db.update_records('test_table', old_records, new_records)
+        expect(db.connection["select * from test_table where test <= 5"].all.size).to eq(2)
+        updated_records = db.connection[:test_table].where(:id => old_records.map {|r| r[:id]}).all
+        expect(updated_records.size).to eq(new_records.size)
+        expect(updated_records).to eq(new_records)
+      end
     end
-
-    db.delete_records(:test_table, {:test => 1..5})
-
-    results = db.connection["select * from test_table"].all
-    results.size.should == 15
-    results.map {|r| r[:test]}.sort.should == (6..20).to_a.sort
-  end
-
-  it "should be able to update records" do
-    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :user => 'postgres')
-    (1..20).each do |i|
-      db.connection.run("insert into test_table (name, test) values ('test', #{i})")
-    end
-
-    old_records = db.connection["select * from test_table where test <= 5"].all
-    new_records = old_records.map {|r| r.merge(:test => r[:test] * 10)}
-    db.update_records('test_table', old_records, new_records)
-    db.connection["select * from test_table where test <= 5"].all.size.should == 0
-    updated_records = db.connection[:test_table].where(:id => old_records.map {|r| r[:id]}).all
-    updated_records.size.should == old_records.size
-    updated_records.should == new_records
-  end
-
-  it "should be able to handle deletions in update records" do
-    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :user => 'postgres')
-    (1..20).each do |i|
-      db.connection.run("insert into test_table (name, test) values ('test', #{i})")
-    end
-
-    old_records = db.connection["select * from test_table where test <= 5"].all
-    new_records = old_records[0..1]
-    db.update_records('test_table', old_records, new_records)
-    db.connection["select * from test_table where test <= 5"].all.size.should == 2
-    updated_records = db.connection[:test_table].where(:id => old_records.map {|r| r[:id]}).all
-    updated_records.size.should == new_records.size
-    updated_records.should == new_records
   end
 end
