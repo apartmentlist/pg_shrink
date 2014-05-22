7c95ec681cace8940760a03e42a8c0c8cae0bb37
Kevin Ball <kmball11@gmail.com>
2014-05-22 10:33:06 -0700

    Get postgres db adapter able to update and delete records

--- lib/pg_shrink/database/postgres.rb
+++ lib/pg_shrink/database/postgres.rb
@@ -45,7 +45,38 @@ module PgShrink
       end
     end
 
+    # TODO:  Do we need to do some checking to make sure new_records has
+    # primary keys that are a strict subset of update_records?
     def update_records(table_name, old_records, new_records)
+      table = self.get_table(table_name)
+      primary_key = table.primary_key
+
+      # no inject so we only create 1 object
+      old_records_by_key = {}
+      old_records.each {|r| old_records_by_key[r[primary_key]] = r}
+
+
+      new_records_by_key = {}
+      new_records.each {|r| new_records_by_key[r[primary_key]] = r}
+
+      deleted_record_ids =  old_records_by_key.keys - new_records_by_key.keys
+      if deleted_record_ids.any?
+        self.delete_records(table_name, {primary_key => deleted_record_ids})
+      end
+
+      # TODO:  Is it worth optimizing this to do bulk updates?  Or are bulk
+      # deletes above good enough?
+      new_records.each do |rec|
+        if old_records_by_key[rec[primary_key]] != rec
+          self.connection.from(table_name).
+               where(primary_key => rec[primary_key]).
+               update(rec)
+        end
+      end
+    end
+
+    def delete_records(table_name, condition_to_delete)
+      self.connection.from(table_name).where(condition_to_delete).delete
     end
   end
 end
--- spec/pg_shrink/database/postgres_spec.rb
+++ spec/pg_shrink/database/postgres_spec.rb
@@ -28,4 +28,47 @@ describe PgShrink::Database::Postgres do
     end
     batch_sizes.should == [5, 5, 5, 5]
   end
+
+  it "should be able to delete records based on a condition" do
+    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :user => 'postgres')
+    (1..20).each do |i|
+      db.connection.run("insert into test_table (name, test) values ('test', #{i})")
+    end
+
+    db.delete_records(:test_table, {:test => 1..5})
+
+    results = db.connection["select * from test_table"].all
+    results.size.should == 15
+    results.map {|r| r[:test]}.sort.should == (6..20).to_a.sort
+  end
+
+  it "should be able to update records" do
+    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :user => 'postgres')
+    (1..20).each do |i|
+      db.connection.run("insert into test_table (name, test) values ('test', #{i})")
+    end
+
+    old_records = db.connection["select * from test_table where test <= 5"].all
+    new_records = old_records.map {|r| r.merge(:test => r[:test] * 10)}
+    db.update_records('test_table', old_records, new_records)
+    db.connection["select * from test_table where test <= 5"].all.size.should == 0
+    updated_records = db.connection[:test_table].where(:id => old_records.map {|r| r[:id]}).all
+    updated_records.size.should == old_records.size
+    updated_records.should == new_records
+  end
+
+  it "should be able to handle deletions in update records" do
+    db =PgShrink::Database::Postgres.new(:database => 'test_pg_shrink', :user => 'postgres')
+    (1..20).each do |i|
+      db.connection.run("insert into test_table (name, test) values ('test', #{i})")
+    end
+
+    old_records = db.connection["select * from test_table where test <= 5"].all
+    new_records = old_records[0..1]
+    db.update_records('test_table', old_records, new_records)
+    db.connection["select * from test_table where test <= 5"].all.size.should == 2
+    updated_records = db.connection[:test_table].where(:id => old_records.map {|r| r[:id]}).all
+    updated_records.size.should == new_records.size
+    updated_records.should == new_records
+  end
 end
