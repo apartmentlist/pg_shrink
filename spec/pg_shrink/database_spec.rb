19c259d6e06b20275d0c73f548af85a18505f2aa
Kevin Ball <kmball11@gmail.com>
2014-05-22 10:43:14 -0700

    Set up database filter_table method and start testing

--- spec/pg_shrink/database_spec.rb
+++ spec/pg_shrink/database_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
 
 describe PgShrink::Database do
-  it "should have some generic database tests" do
-    pending
+  it "should yield a table to filter_table" do
+    db = PgShrink::Database.new
+    db.filter_table(:test_table) do |tb|
+      tb.is_a?(PgShrink::Table).should == true
+      tb.database.should == db
+      tb.table_name.should == :test_table
+    end
+  end
+
+  it "should allow options to set a different primary_key in filter_table" do
+    db = PgShrink::Database.new
+    db.filter_table(:test_table, :primary_key => :foo) do |tb|
+      tb.is_a?(PgShrink::Table).should == true
+      tb.database.should == db
+      tb.primary_key.should == :foo
+    end
+  end
+
+  it "should retain options in later invocations" do
+    db = PgShrink::Database.new
+    db.filter_table(:test_table, :primary_key => :foo) do |tb|
+      tb.is_a?(PgShrink::Table).should == true
+      tb.database.should == db
+      tb.primary_key.should == :foo
+    end
+    db.filter_table(:test_table) do |tb|
+      tb.primary_key.should == :foo
+    end
   end
 end
